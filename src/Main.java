c2a3e167cb90e48e0b489d92df6bc768a2a17e69
Moises Muiña <mochyfm@gmail.com>
2022-05-26 09:54:28 +0100

    Feature: Creada la prueba de la clase SuperList en el Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import com.ud5problema1.superlist.SuperList;
+
 public class Main {
     public static void main(String[] args) {
+
+        SuperList<Integer> miLista = new SuperList<>();
+
+        miLista.addElement(69);
+        miLista.addElement(5);
+        miLista.addElement(42069);
+        miLista.addElement(695);
+
+        System.out.println(miLista + "\n");
+        miLista.showElements();
+        System.out.println("\n" + " ---------- " + "\n");
+        miLista.showReversedElements();
+        System.out.println("\n" + " ----- Eliminando el elemento en la posicion 3 ----- " + "\n");
+
+        miLista.removeElement(3);
+
+        miLista.showElements();
+
+
 
 
 
--- src/com/ud5problema1/superlist/SuperList.java
+++ src/com/ud5problema1/superlist/SuperList.java
@@ -17,15 +17,17 @@ public class SuperList<T> extends ListManager<T> {
     @Override
     public void showElements() {
         Iterator<T> it = list.iterator();
+        int contador = 1;
         while (it.hasNext()) {
-            System.out.println(it.next());
+            System.out.println(contador + ". " + it.next());
+            contador++;
         }
     }
 
     @Override
     public void showReversedElements() {
-        for (int i = list.size() - 1; i > 0; i--) {
-            System.out.println(list.get(i));
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.println((i + 1) + ". " + list.get(i));
         }
     }
 }
